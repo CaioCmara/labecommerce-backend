457ae0b89fb8cf32f2667014e9334d752c9269f4
CaioCmara <caio1801@gmail.com>
2023-01-09 14:12:28 -0300

    type2

--- build/index.js
+++ build/index.js
@@ -1,7 +1,54 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const database_1 = require("./database");
-console.log(database_1.users);
-console.log(database_1.products);
-console.log(database_1.purchases);
+function helloTurna() {
+    console.log("Boa tarde");
+}
+helloTurna();
+const client = {
+    id: "34fdaf",
+    name: "caio",
+    email: "coaksod@gmail.com",
+    password: "o31231",
+    role: "ADMIN"
+};
+var Role;
+(function (Role) {
+    Role["ADMIN"] = "ADMIN";
+    Role["NORMAL"] = "NORMAL";
+})(Role || (Role = {}));
+const novoCliente = {
+    id: "jkvaoi3",
+    name: "Pastor",
+    email: "pastofa@gmail.com",
+    password: "pastor3123",
+    role: Role.ADMIN
+};
+console.table(novoCliente);
+const normalAccountArray = [];
+const userNormal = {
+    id: "sa2314",
+    name: "Cainho",
+    email: "casod@contato.com",
+    password: "31caio10",
+    account: "CaioPakas",
+    role: Role.NORMAL,
+    permission: false
+};
+const userAdmin = {
+    id: "casda3",
+    name: "vascta",
+    email: "dsda3vc@contato.com",
+    password: "vastasd",
+    account: "vavapapa",
+    role: Role.ADMIN,
+    permission: true
+};
+const pessoaCliente = {
+    id: "casfa",
+    name: "tar3asdd",
+    email: "casbaca@contato.com",
+    password: "casw1ca",
+    role: Role.ADMIN,
+};
+normalAccountArray.push(userNormal);
+console.table(normalAccountArray);
 //# sourceMappingURL=index.js.map
